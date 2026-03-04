9aef3b371f61bd9841f772dc8f25228045cc5f29
saikumar308 <SAIKUMARDUSA308@GMAIL.COM>
2026-03-05 02:42:46 +0530

    update index.js
    Updated the server to use a dynamic port and fixed a typo in the search query handling.

--- index.js
+++ index.js
@@ -137,6 +137,8 @@ app.get("/api/movies/:id", (req, res) => {
     }
     res.json(movie);
 });
-app.listen(5000,()=>{
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+  console.log("Server is running on port ${PORT}");
+
 });
